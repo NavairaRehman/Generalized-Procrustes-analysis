a641b33ea7e717fe8ba16b1c69d5d863a21e75e5
Navaira Rehman <nrehman.bese21seecs@student.nust.edu.pk>
2025-01-21 00:49:12 +0500

    chore: add debug statements and code to save files

--- GPA_PCA.m
+++ GPA_PCA.m
@@ -5,28 +5,38 @@
 %   Email - pulak.isi@gmail.com 
 
 %% Read datasets  
-data_path = '300W/01_Indoor/'; 
+data_path = '/Users/navairarehman/Documents/FYP/3d preprocess/Procrustes/300w_cropped/01_Indoor/'; 
 filelist = dir(data_path);
+
+%Calculating the # of datasample (images and their corresponding pts files)
 datasize = numel(filelist)/2 -1; 
 
 % Note that the images are of different size 
+% the following lines if image has the same dimensions. 
 % sz = size(imread([data_path, filelist(3).name])); 
 % images = zeros([datasize, sz]); 
 
-flag_display = 1; % You can choose this to be 0 if you dont want to display 
-images = cell(1, datasize); 
+flag_display = 0; % You can choose this to be 0 if you dont want to display 
+images = cell(1, datasize); %using a cell array for images to store different dimensions of image data. 
 
+%reading a pts file to get the no. of points/landmarks. They are stored in
+%the header of the pts files of the test dataset they have used.
+%68 in this case
 fileID = fopen([data_path, filelist(4).name], 'r'); 
 fscanf(fileID, '%s', 3); 
 no_points = fscanf(fileID, '%d\n', 1); 
 fclose(fileID);
 
+%3 dim array to store x and y coordinates of all the points of each
+%datapoint in the dataset.
 points = zeros([datasize, 2, no_points]); 
 
+%readign the image and pts file and populating the points array and image
+%cell array
 for i = 1:datasize 
-    
+
     images{i} = imread([data_path, filelist(2*i+1).name]); 
-    
+
     fileID = fopen([data_path, filelist(2*i+2).name], 'r'); 
     fscanf(fileID, '%s', 3); 
     no_points = fscanf(fileID, '%d\n', 1); 
@@ -34,13 +44,15 @@ for i = 1:datasize
     fscanf(fileID, '%s', 1); 
     points(i, :, :) = reshape(fscanf(fileID, '%f', 2*no_points), [2, no_points]); 
     disp(i/datasize); 
-     
+    disp("done")
+
 % Plot : To check if the readings are correct 
     if flag_display
-        imshow( images{i} ); hold on; 
+        imshow( images{i} ); 
+        hold on; 
         plot(squeeze(points(i, 1, :)),  squeeze(points(i, 2, :)), 'r*'); 
         hold off; 
-        pause; 
+        %pause; %comment this out. It enables viewing the landmarks on each photo if flag_display in on. 
     end
 
 end 
@@ -48,17 +60,17 @@ end
 % save('points.mat', 'points'); 
 % Procrustes analysis https://en.wikipedia.org/wiki/Procrustes_analysis 
 % load('points.mat'); 
-% Translation 
+% Translation (so that the mean of the points is cetered at the origin)
 
 points_mean = repmat(mean(points, 3), [1, 1, no_points]); 
 points = points - points_mean; 
 
-%% Scaling 
+%% Scaling (to have the same size by adjusting for differences distance)
 
 points_scale = sqrt(sum(sum(points.^2, 3), 2)/no_points); 
 points = points./repmat(points_scale, [1, 2, no_points]); 
 
-%% Rotation 
+%% Rotation (to minimize the distance and difference between the points)
 
 theta = atan(squeeze(sum(points(:, 1, :).*repmat(points(1, 2, :), [datasize, 1, 1]) - points(1, 2, :).*repmat(points(1, 1, :), [datasize, 1, 1]), 3))...
     ./squeeze(sum(points(:, 1, :).*repmat(points(1, 1, :), [datasize, 1, 1]) + points(1, 2, :).*repmat(points(1, 2, :), [datasize, 1, 1]), 3))); 
@@ -67,13 +79,16 @@ R1 = [cos(theta), -sin(theta)];
 R2 = [sin(theta), cos(theta)]; 
 
 points_transformed = cat(2, sum(points.*repmat(R1, [1, 1, no_points]), 2), sum(points.*repmat(R2, [1, 1, no_points]), 2)); 
+disp("procrustes done")
+disp(points_transformed)
 
 % Checking again if the things are going well 
+%displaying the transformed points after translation, scaling and rotation.
 if flag_display
     for i = 1:datasize 
         plot(squeeze(points_transformed(i, 1, :)), - squeeze(points_transformed(i, 2, :)), 'r*'); 
         hold off; 
-        pause;  
+        %pause;  
     end
 end
 
@@ -83,20 +98,23 @@ end
 
 % load('points.mat'); 
 % Translation 
-no_points = size(points, 3);  
-datasize = size(points, 1); 
+disp("Performing GPA")
+no_points = size(points, 3);   
+datasize = size(points, 1);   
 
 points_mean = mean(points, 3); 
 points_mean = repmat(points_mean, [1, 1, no_points]); 
 
 points = points - points_mean; 
 
+
 N = 1000;                                % Maximum number of iterations 
-epsln = 0.01;                           % Quality Parameter 
-C_hat = squeeze(points(1, :, :));
+epsln = 0.01;                            % Quality Parameter 
+C_hat = squeeze(points(1, :, :));        %initial estimate of the mean shape. Taken as the first image here.
 Error_plot = zeros(N, 1); 
 Error_plot(1) = 1; 
 iter = 1; 
+disp("going into GPA loop")
 while (Error_plot(iter) > epsln && iter < N) 
     C = C_hat; 
     % Compute similarity transformations between the images and mean shape 
@@ -125,9 +143,9 @@ points_transformed = points;
 
 for i = 1:datasize
     X = squeeze(points(i, :, :)); 
-    s = sqrt(sum(C_hat(:).^2) / sum(X(:).^2)); 
+    s = sqrt(sum(C_hat(:).^2) / sum(X(:).^2)); %scaling factor caluclated for each image  
     M = C_hat*X';
-    R = M*(M'*M)^(-0.5); 
+    R = M*(M'*M)^(-0.5); %rotation matrix for each image
     points_transformed(i, :, :) = s*R*squeeze(points(i, :, :)); 
 end
 
@@ -138,35 +156,63 @@ if flag_display
         plot(C_hat(1, :), - C_hat(2, :), 'g*'); 
         axis([-0.15 0.15 -0.15 0.15]); 
         hold off; 
-        pause; 
+        %pause; 
     end
 end
+% 
+% %% Principle component Analysis of shapes 
+% X = reshape(points_transformed, [datasize, 2*no_points]); 
+% X = bsxfun(@minus,X,mean(X));
+% % [coeff,score,latent] = pca(X); 
+% [V, D] = eig(X'*X); 
+% D=diag(D);
+% [D_sorted, id]=sort(D, 'descend'); % sort according to the magnitude of eigenvalues 
+% D_sum = cumsum(D_sorted)/sum(D_sorted); 
+% id_c = find(D_sum < 0.98);        % keep 98% data 
+% 
+% M = V(:, id(id_c));               % Eigenvectors corresponding to large eigen values 
+% Y = X*M;                          % Data projected on the eigenspaces 
+% 
+% eigenshapes = reshape(M, [numel(id_c), 2, no_points]); % Computing Eigen Shapes 
+% 
+% % Plotting the Eigen Shapes 
+% if flag_display
+%     for i = 1:datasize 
+%         plot(squeeze(eigenshapes(i, 1, :)), - squeeze(eigenshapes(i, 2, :)), 'r*'); hold on; 
+%         plot(C_hat(1, :), - C_hat(2, :), 'g*'); 
+%         axis([-0.15 0.15 -0.15 0.15]); 
+%         hold off; 
+%         pause; 
+%     end
+% end
+% X_bar = Y*M';                    % Back to the original shape 
+% 
+% disp(norm(X - X_bar)); 
 
-%% Principle component Analysis of shapes 
-X = reshape(points_transformed, [datasize, 2*no_points]); 
-X = bsxfun(@minus,X,mean(X));
-% [coeff,score,latent] = pca(X); 
-[V, D] = eig(X'*X); 
-D=diag(D);
-[D_sorted, id]=sort(D, 'descend'); % sort according to the magnitude of eigenvalues 
-D_sum = cumsum(D_sorted)/sum(D_sorted); 
-id_c = find(D_sum < 0.98);        % keep 98% data 
+% saving the points in a pts file 
+% Define output path where the transformed pts files will be saved
+output_path = '/Users/navairarehman/Documents/FYP/3d preprocess/Procrustes/300w_cropped/aligned_pts/';
 
-M = V(:, id(id_c));               % Eigenvectors corresponding to large eigen values 
-Y = X*M;                          % Data projected on the eigenspaces 
-
-eigenshapes = reshape(M, [numel(id_c), 2, no_points]); % Computing Eigen Shapes 
-
-% Plotting the Eigen Shapes 
-if flag_display
-    for i = 1:datasize 
-        plot(squeeze(eigenshapes(i, 1, :)), - squeeze(eigenshapes(i, 2, :)), 'r*'); hold on; 
-        plot(C_hat(1, :), - C_hat(2, :), 'g*'); 
-        axis([-0.15 0.15 -0.15 0.15]); 
-        hold off; 
-        pause; 
-    end
+% Loop through all the images and save the transformed points into new .pts files
+for i = 1:datasize
+    % Create the new .pts file name (you can modify this as per your requirement)
+    [~, filename, ~] = fileparts(filelist(2*i+2).name); % Extract filename without extension
+    output_filename = [filename, '_aligned.pts']; % Create new filename for aligned points
+    
+    % Open the new .pts file for writing
+    fileID = fopen([output_path, output_filename], 'w');
+    
+    % Write the header
+    fprintf(fileID, 'version: 1\n');
+    fprintf(fileID, 'n_points: %d\n', no_points);
+    fprintf(fileID, 'point\n');
+    
+    % Write the transformed points to the file
+    transformed_points = squeeze(points_transformed(i, :, :)); % Extract the transformed points for the current image
+    fprintf(fileID, '%f %f\n', transformed_points); % Write the points as x y coordinates
+    
+    % Close the file
+    fclose(fileID);
+    
+    disp(['Saved aligned points for image ', num2str(i)]);
 end
-X_bar = Y*M';                    % Back to the original shape 
-
-disp(norm(X - X_bar)); 
